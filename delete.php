79c2caced06520296fbe563bfc794dd7c09150b3
Suphitcha03 <koomza097@gmail.com>
2026-05-30 15:11:47 +0700

    update code

--- delete.php
+++ delete.php
@@ -1,13 +1,16 @@
 <?php
 require_once "db/connect.php";
 //ถ้าไม่มีการส่งไอดีมาให้วิ่งกลับไปหน้าเเรก อัตโนมัติ
-if(!isset($_GET["id"])){
+if(!isset($_POST["id"])){
     header("Location:index.php");
+    exit();
 }else{
     $id=$_GET["id"];
     $result = $controller->delete($id);
+    
     if($result){
         header("Location:index.php");
+        exit();
     }
 }
 
